bccf36bb631158d6a9abac1166d646a29645725f
loiolaarthur4-max <loiolaarthur4@gmail.com>
2026-09-04 16:57:20 -0300

    Update API key retrieval method in Uly.py
    Replaced the method of obtaining the API key with a hardcoded value.

--- Uly.py
+++ Uly.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Agente IA Gemini", page_icon="🤖")
 st.title("🤖 Meu Agente de IA com Gemini")
 
 # Obter a API Key das variáveis de ambiente (Secrets)
-api_key = os.environ.get("GEMINI_API_KEY")
+api_key = os.environ.get("AQ.Ab8RN6LWPyXLA3YlEEuEa5eOaWAxvaMW9dhjh_9BpCigjGdqfA")
 
 if not api_key:
     st.error("Chave de API do Gemini não encontrada! Configure a variável GEMINI_API_KEY.")
